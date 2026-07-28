dea4a5dac8816d559d1a556ec7414d0a4047d5bf
IanLulu <ilulu2@uic.edu>
2026-07-27 19:19:41 -0500

    feat: add OpenRouter API setup

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv() # load environment variables from .env
+api_key = os.environ.get("OPENROUTER_API_KEY") # read API key
+if api_key is None:
+	raise RuntimeError("API key not found.") # error message if not found
+
+# create an OpenAI client:
+client = OpenAI(
+     base_url="https://openrouter.ai/api/v1",
+     api_key=api_key,
+)
+
 def main():
-    print("Hello from ai-coding-agent!")
+    # print("Hello from ai-coding-agent!")
+    model: str = "openrouter/free" # specific LLM being used
+    messages: list[dict] = [
+         {
+              "role": "user",
+              "content": "Hello World!",
+         }
+    ]
+
+    chat = client.chat.completions.create(model=model, messages=messages)
+    print(chat.choices[0].message.content)
 
 
 if __name__ == "__main__":
